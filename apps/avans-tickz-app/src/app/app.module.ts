6187f209c1f9a3e9187b2c203404b75dc6d28ec6
Juliet van Bezooijen <jay.vanbezooijen@student.avans.nl>
2022-11-19 15:40:00 +0100

    Installed npm angular forms

--- apps/avans-tickz-app/src/app/app.module.ts
+++ apps/avans-tickz-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RoutingModule } from './app.routes';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { UserComponent } from './pages/entity/user/user.component';
 import { NavComponent } from './shared/nav/nav.component';
 import { DetailComponent } from './pages/entity/user/detail/detail.component';
 import { ConcertComponent } from './pages/entity/concert/concert.component';
 import { EditComponent } from './pages/entity/user/edit/edit.component';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +22,7 @@ import { EditComponent } from './pages/entity/user/edit/edit.component';
     ConcertComponent,
     EditComponent,
   ],
-  imports: [BrowserModule, RoutingModule, NgbModule],
+  imports: [BrowserModule, RoutingModule, NgbModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
