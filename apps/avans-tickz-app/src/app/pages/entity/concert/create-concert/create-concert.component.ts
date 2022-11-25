10b18bf03310ac9754df42e076d633a846fcf40c
Juliet van Bezooijen <jay.vanbezooijen@student.avans.nl>
2022-11-25 22:27:51 +0100

    Made detail page and styled links

--- apps/avans-tickz-app/src/app/pages/entity/concert/create-concert/create-concert.component.ts
+++ apps/avans-tickz-app/src/app/pages/entity/concert/create-concert/create-concert.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Concert } from '../concert.model';
+import { ConcertService } from '../concert.service';
 
 @Component({
   selector: 'avans-tickz-create-concert',
@@ -8,12 +10,13 @@ import { Concert } from '../concert.model';
 })
 export class CreateConcertComponent implements OnInit {
 
-  constructor() { }
+  constructor(private concertService: ConcertService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   createConcert(concert: Concert): void{
-
+    this.concertService.concertList.push(concert);
+    this.router.navigate(['./concerts']);
   }
 }
