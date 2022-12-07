84819f5e20b7c7cb8c3ff6f5000c379f36a384f8
Juliet van Bezooijen <jay.vanbezooijen@student.avans.nl>
2022-12-07 18:53:14 +0100

    deleted comments

--- apps/avans-tickz-app/src/app/pages/auth/auth.service.ts
+++ apps/avans-tickz-app/src/app/pages/auth/auth.service.ts
@@ -114,26 +114,6 @@ export class AuthService {
     }
   }
 
-  // createUser(user: User): Observable<any> {
-  //   console.log('User create aangeroepen');
-  //   this.user = { ...user };
-  //   this.user.userId = ++this.userId;
-  //   this.userList.push(this.user);
-  //   console.log(`Nieuwe user toegevoegd met ID ${this.userId}`);
-  //   return of({
-  //     status: 201,
-  //     message: 'success',
-  //   });
-  // }
-
-  // updateUser(user?: User): Observable<any> {
-  //   console.log('Update User aangeroepen');
-  //   return of({
-  //     status: 201,
-  //     message: 'success',
-  //   });
-  // }
-
   // private handleError(error: HttpErrorResponse): Observable<any> {
   //   console.log(error);
   //   // return an error observable with a user-facing error message
